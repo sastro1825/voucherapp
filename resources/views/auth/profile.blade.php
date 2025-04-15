bebc96f50c3e2ca57ef63ba2d6bff05ab04787b0
ridho <ridhofajar448@gmail.com>
2025-04-15 10:54:25 +0700

    fix bug profil merchant

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -5,42 +5,64 @@
 @section('sidebar-title')
     <div class="flex flex-col items-center mb-4">
         <img src="{{ asset('images/FT.png') }}" alt="Logo" class="h-12 w-auto mb-2">
-        <span class="text-lg font-semibold text-gray-800 dark:text-gray-200">Admin Panel</span>
+        <span class="text-lg font-semibold text-gray-800 dark:text-gray-200">
+            {{ auth()->user()->role === 'admin' ? 'Admin Panel' : 'Merchant Panel' }}
+        </span>
     </div>
 @endsection
 
 @section('sidebar-menu')
-    <li>
-        <a href="{{ route('admin.create-voucher') }}" class="block p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">Create Voucher</a>
-    </li>
-    <li>
-        <a href="{{ route('admin.create-merchant') }}" class="block p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">Create Merchant</a>
-    </li>
-    <li>
-        <a href="{{ route('admin.vouchers') }}" class="block p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">View All Vouchers</a>
-    </li>
-    <li x-data="{ showSetting: false }">
-        <button @click="showSetting = !showSetting" class="block p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded w-full text-left flex justify-between items-center">
-            Setting
-            <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-            </svg>
-        </button>
-        <ul x-show="showSetting" class="pl-4">
-            <li>
-                <a href="{{ route('profile') }}" class="block p-2 bg-gray-200 dark:bg-gray-700 rounded">Profil</a>
-            </li>
-            <li>
-                <a href="{{ route('admin.update-company') }}" class="block p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">Update Company Name</a>
-            </li>
-        </ul>
-    </li>
-    <li>
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <button type="submit" class="block w-full text-left p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">Logout</button>
-        </form>
-    </li>
+    @if (auth()->user()->role === 'admin')
+        <!-- Sidebar untuk Admin -->
+        <li>
+            <a href="{{ route('admin.create-voucher') }}" class="block p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">Create Voucher</a>
+        </li>
+        <li>
+            <a href="{{ route('admin.create-merchant') }}" class="block p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">Create Merchant</a>
+        </li>
+        <li>
+            <a href="{{ route('admin.vouchers') }}" class="block p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">View All Vouchers</a>
+        </li>
+        <li x-data="{ showSetting: false }">
+            <button @click="showSetting = !showSetting" class="block p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded w-full text-left flex justify-between items-center">
+                Setting
+                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                </svg>
+            </button>
+            <ul x-show="showSetting" class="pl-4">
+                <li>
+                    <a href="{{ route('profile') }}" class="block p-2 bg-gray-200 dark:bg-gray-700 rounded">Profil</a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.update-company') }}" class="block p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">Update Company Name</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit" class="block w-full text-left p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">Logout</button>
+            </form>
+        </li>
+    @else
+        <!-- Sidebar untuk Merchant -->
+        <li>
+            <a href="{{ route('merchant.dashboard') }}#redeem-voucher" class="block p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">Redeem Voucher</a>
+        </li>
+        <li>
+            <a href="{{ route('merchant.redeemed-vouchers') }}" class="block p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">View Redeemed Vouchers</a>
+        </li>
+        <li>
+            <a href="{{ route('profile') }}" class="block p-2 bg-gray-200 dark:bg-gray-700 rounded">Profil</a>
+        </li>
+        <li>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit" class="block w-full text-left p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded">Logout</button>
+            </form>
+        </li>
+    @endif
 @endsection
 
 @section('content')
