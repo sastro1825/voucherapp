90e387c7e979ca1b2aae16af95353d7790b5d1dc
nafispratama04 <nafispratamaputra@gmail.com>
2025-04-19 10:19:00 +0700

    spam

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -51,14 +51,8 @@
 
     <!-- Notification -->
     @if (session('success'))
-        <div class="bg-green-100 text-green-700 p-4 rounded mb-4">
-            {{ session('success') }}
-        </div>
     @endif
     @if (session('error'))
-        <div class="bg-red-100 text-red-700 p-4 rounded mb-4">
-            {{ session('error') }}
-        </div>
     @endif
 
     <!-- Search Form -->
